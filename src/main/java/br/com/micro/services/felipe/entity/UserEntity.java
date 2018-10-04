a605a6c6719d852d6b4789be5a416e299228f928
Felipe Rodrigues <felipe.rodrigues@keyrus.com.br>
2018-10-04 00:24:05 -0300

    fix import

--- src/main/java/br/com/micro/services/felipe/entity/UserEntity.java
+++ src/main/java/br/com/micro/services/felipe/entity/UserEntity.java
@@ -5,6 +5,7 @@ package br.com.micro.services.felipe.entity;
 import org.springframework.data.annotation.Id;
 
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import lombok.Data;
 
